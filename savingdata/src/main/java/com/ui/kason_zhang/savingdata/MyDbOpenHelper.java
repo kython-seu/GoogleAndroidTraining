16d9e873f9d737528b6253fd742857a025c7e4fc
kython_seu <flowercomeout@163.com>
2016-09-05 18:34:48 +0800

    update MyDbOpenHelper

--- savingdata/src/main/java/com/ui/kason_zhang/savingdata/MyDbOpenHelper.java
+++ savingdata/src/main/java/com/ui/kason_zhang/savingdata/MyDbOpenHelper.java
@@ -3,13 +3,14 @@ package com.ui.kason_zhang.savingdata;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
+import android.util.Log;
 
 /**
  * Created by kason_zhang on 9/1/2016.
  */
 public class MyDbOpenHelper extends SQLiteOpenHelper{
 
+    private static final String TAG = "MyDbOpenHelper";
     private Context mContext;
     private static final String CREATE_BOOK = "create table Book ("+
             "id integer primary key autoincrement,"+
@@ -17,6 +18,10 @@ public class MyDbOpenHelper extends SQLiteOpenHelper{
             "price real,"+
             "pages integer,"+
             "name text)";
+    private static final String CREATE_CATEGORY = "create table Category ("
+            +"id integer primary key autoincrement,"
+            +"category_name text,"
+            +"category_code integer)";
     public MyDbOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
         mContext = context;
@@ -25,11 +30,15 @@ public class MyDbOpenHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
-        Toast.makeText(mContext,"Database has created successfully",Toast.LENGTH_LONG).show();
+        db.execSQL(CREATE_CATEGORY);
+        Log.i(TAG, "Database has created successfully");
+        //Toast.makeText(mContext,"Database has created successfully",Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists table Book");
+        db.execSQL("drop table if exists table Category");
+        onCreate(db);
     }
 }
