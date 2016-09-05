9f78f35ced586351909177484e17b80d29390f7c
kython_seu <flowercomeout@163.com>
2016-09-05 14:59:41 +0800

    load the bitmap efficiently

---
+++ displaybitmapefficiently/src/main/java/com/ui/kason_zhang/displaybitmapefficiently/BitMapWorkTask.java
@@ -0,0 +1,7 @@
+package com.ui.kason_zhang.displaybitmapefficiently;
+
+/**
+ * Created by kason_zhang on 9/5/2016.
+ */
+public class BitMapWorkTask {
+}
---
+++ displaybitmapefficiently/src/main/java/com/ui/kason_zhang/displaybitmapefficiently/MainActivity.java
@@ -0,0 +1,66 @@
+package com.ui.kason_zhang.displaybitmapefficiently;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ImageView imageView;
+    private ImageView secondImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        secondImage=(ImageView)findViewById(R.id.secondImage);
+        secondImage.setImageResource(R.drawable.flower);
+        imageView = (ImageView)findViewById(R.id.myImage);
+
+
+        getImageByTargetSize();
+    }
+
+    private void getImageByTargetSize() {
+        BitmapFactory.Options mOptions = new BitmapFactory.Options();
+        mOptions.inJustDecodeBounds = true;//set the image not into the internal memory;
+        BitmapFactory.decodeResource(getResources(), R.drawable.flower, mOptions);
+        int imageHeight = mOptions.outHeight;
+        int imageWidth = mOptions.outWidth;
+        String imageType = mOptions.outMimeType;
+        Log.i(TAG, "onCreate: imageHeight:"+imageHeight+" --imageWidth:"+imageWidth
+        +"---imageType:"+imageType);
+        int sampleSize = calculateSampleSize(mOptions,100,100);
+        Log.i(TAG, "onCreate: the sampleSize is"+sampleSize);
+
+        mOptions.inSampleSize = sampleSize;
+        mOptions.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.flower, mOptions);
+
+        Log.i(TAG, "onCreate: "+bitmap.getHeight()+"-----Option Getheight:"+mOptions.outHeight);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private int calculateSampleSize(BitmapFactory.Options option,
+                                    int targetHeight,int tartgetWidth){
+        int sampleSize = 1;
+        int height = option.outHeight;
+        int width = option.outWidth;
+
+        if(height>targetHeight||width>tartgetWidth){
+            final int halfheight = height/2;
+            final int halfwidth = width/2;
+            while(halfheight/sampleSize>targetHeight&&halfwidth/sampleSize>tartgetWidth){
+                Log.i(TAG, "calculateSampleSize: "+halfheight/sampleSize+" : "+targetHeight
+                +"-----"+halfwidth/sampleSize+" : "+tartgetWidth);
+                sampleSize = sampleSize*2;
+            }
+        }
+
+        return sampleSize;
+    }
+}
