6cc8f6470d5d314128bde1edcaa620a806e7239b
kython-seu <flowercomeout@163.com>
2016-09-06 15:12:51 +0800

    show the progress in the notification

--- notifytheuser/src/main/java/com/ui/kason_zhang/notifytheuser/MainActivity.java
+++ notifytheuser/src/main/java/com/ui/kason_zhang/notifytheuser/MainActivity.java
@@ -14,6 +14,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //displayNotification();
+        displayProgressNotification();
+    }
+
+    private void displayProgressNotification() {
+        final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Download")
+                .setContentText("Download in progress");
+        final NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int step = 0;
+                for(int i=0;i<100;i+=5){
+                    step = i;
+                    mBuilder.setProgress(100,step,false);
+                    notificationManager.notify(1,mBuilder.build());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                // When the loop is finished, updates the notification
+                mBuilder.setContentText("Download complete")
+                        // Removes the progress bar
+                        .setProgress(0,0,false);
+                notificationManager.notify(1, mBuilder.build());
+            }
+        }).start();
+    }
+
+    private void displayNotification() {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
         mBuilder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("My notification")
@@ -25,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 ,intent,PendingIntent.FLAG_UPDATE_CURRENT);
 
         mBuilder.setContentIntent(pendingIntent);
+
 
         NotificationManager notificationManager = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
