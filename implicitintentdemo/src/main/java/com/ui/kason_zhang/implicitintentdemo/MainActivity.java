27fb42977400dfc7d5321999d006c4ff7f0d4a26
kython-seu <flowercomeout@163.com>
2016-09-06 15:28:55 +0800

    explicit Intent

---
+++ implicitintentdemo/src/main/java/com/ui/kason_zhang/implicitintentdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ui.kason_zhang.implicitintentdemo;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                ComponentName componentName = new ComponentName("com.ui.kason_zhang.implicitintentdemo"
+                ,"com.ui.kason_zhang.implicitintentdemo.SecondActivity");
+                intent.setComponent(componentName);
+                startActivity(intent);
+            }
+        });
+    }
+}
