1dbcb004d439a937f1e1d4da455389bbcb718876
kython-seu <flowercomeout@163.com>
2016-09-27 15:00:15 +0800

    recyclerView demo init

---
+++ recyclerviewdemo/src/main/java/adapters/CustomRecyclerViewAdapter.java
@@ -0,0 +1,90 @@
+package adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by kason_zhang on 9/27/2016.
+ */
+
+public class CustomRecyclerViewAdapter<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final int TYPE_HEADER = 0;  //说明是带有Header的
+    public static final int TYPE_FOOTER = 1;  //说明是带有Footer的
+    public static final int TYPE_NORMAL = 2;  //说明是不带有header和footer的
+    //获取从Activity中传递过来每个item的数据集合
+    private List<T> mDatas;
+    private Context mContext;
+    private View mHeaderView;
+    private View mFooterView;
+    //HeaderView和FooterView的get和set函数
+    public View getHeaderView() {
+        return mHeaderView;
+    }
+    public void setHeaderView(View headerView) {
+        mHeaderView = headerView;
+        notifyItemInserted(0);
+    }
+    public View getFooterView() {
+        return mFooterView;
+    }
+    public void setFooterView(View footerView) {
+        mFooterView = footerView;
+        notifyItemInserted(getItemCount()-1);
+    }
+    public CustomRecyclerViewAdapter(Context context,List<T> mDatas){
+        this.mContext = mContext;
+        this.mDatas = mDatas;
+    }
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+    /** 重写这个方法，很重要，是加入Header和Footer的关键，我们通过判断item的类型，从而绑定不同的view    * */
+    @Override
+    public int getItemViewType(int position) {
+        //return super.getItemViewType(position);
+        if (mHeaderView == null && mFooterView == null){
+            return TYPE_NORMAL;
+        }
+        if (position == 0){
+            //第一个item应该加载Header
+            return TYPE_HEADER;
+        }
+        if (position == getItemCount()-1){
+            //最后一个,应该加载Footer
+            return TYPE_FOOTER;
+        }
+        return TYPE_NORMAL;
+    }
+
+    //在这里面加载ListView中的每个item的布局
+    class CustomViewHolder extends RecyclerView.ViewHolder{
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+    //返回View中Item的个数，这个时候，总的个数应该是ListView中Item的个数加上HeaderView和FooterView
+    @Override
+    public int getItemCount() {
+        if(mHeaderView == null && mFooterView == null){
+            return mDatas.size();
+        }else if(mHeaderView == null && mFooterView != null){
+            return mDatas.size() + 1;
+        }else if (mHeaderView != null && mFooterView == null){
+            return mDatas.size() + 1;
+        }else {
+            return mDatas.size() + 2;
+        }
+    }
+}
